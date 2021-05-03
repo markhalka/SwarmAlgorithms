400ce64533fc914614860e5390ddcab3d5e8d6f2
markhalka <32530193+markhalka@users.noreply.github.com>
2021-05-02 20:54:55 -0400

    Add files via upload

---
+++ Localization/Neighborhood.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+
+// Neighborhood
+// this class contains a group of robots that all share the same coordinate system
+public class Neighborhood
+{
+    public List<Robot> neighbours;
+
+    // Neighborhood(neighbours) creates a new neighborhood object with the givne parameters
+    public Neighborhood(List<Robot> neighbours)
+    {
+        this.neighbours = neighbours;
+    }
+}
